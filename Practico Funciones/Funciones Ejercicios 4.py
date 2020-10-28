7abf611447c88300a668387ff3ea800d19f3e910
Josema <josemadlcastillot@gmail.com>
2020-10-27 20:20:21 -0400

    Funciones Ejercicio 4

---
+++ Practico Funciones/Funciones Ejercicios 4.py
@@ -0,0 +1,9 @@
+#Escribir una función que tome un carácter y devuelva True si es una vocal, de lo contrario devuelve False.
+def vocal(letra):
+    if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+        print(letra, " = True")
+    else:
+        print(letra, " = False")
+
+letra = input("Letra: ")
+vocal(letra)
